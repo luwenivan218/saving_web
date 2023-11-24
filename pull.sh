5f41104c1a04af4f5d5436f4398808c4a259be3d
luwenivan218 <luwenivan218@outlook.com>
2023-11-24 16:40:42 +0800

    update sha

--- pull.sh
+++ pull.sh
@@ -3,6 +3,7 @@
 . /etc/profile
 . ~/.bash_profile
 cd /home/docker-compose/nginx/html/static/saving_web;
-git reset --hard HEAD
+git fetch --all &&  git reset --hard origin/master && git pull && python changeToHtml.py;
+#git reset --hard HEAD
 #git pull;
-python changeToHtml.py;
+# python changeToHtml.py;
